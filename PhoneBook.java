7a3a7c49e47352b0efb7a39b8df4ca7a72b34cc0
Mikhail T <Mikola301@mail.ru>
2023-11-23 01:24:03 +0700

    добавлена сортировка списка

--- PhoneBook.java
+++ PhoneBook.java
@@ -7,18 +7,26 @@ public class PhoneBook {
         addContact(phoneBook, "Петров", "2-15-83");
         addContact(phoneBook, "Смирнов", "2-15-80");
         addContact(phoneBook, "Попов", "2-11-82");
-        addContact(phoneBook, "Соколов",  "2-13-90");
-        addContact(phoneBook, "Попов",  "2-14-79");
-        addContact(phoneBook, "Сидоров",  "2-10-60");
-        System.out.println(phoneBook);
+        addContact(phoneBook, "Соколов", "2-13-90");
+        addContact(phoneBook, "Попов", "2-14-79");
+        addContact(phoneBook, "Сидоров", "2-10-60");
+        // System.out.println(phoneBook);
+        printAndSortBook(phoneBook);
     }
 
-
+    // метод добовления в лист
     public static void addContact(HashMap<String, List<String>> phoneBook, String lastName, String phone) {
         List<String> phones = phoneBook.getOrDefault(lastName, new ArrayList<>());
         phones.add(phone);
         phoneBook.put(lastName, phones);
     }
-    
-    // System.out.println(phoneBook);
+
+    public static void printAndSortBook(HashMap<String, List<String>> phoneBook) {
+        List<Map.Entry<String, List<String>>> sortedList = new ArrayList<>(phoneBook.entrySet());
+        sortedList.sort((a, b) -> Integer.compare(b.getValue().size(), a.getValue().size()));
+        for (Map.Entry<String, List<String>> entry : sortedList) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+    }
 }
